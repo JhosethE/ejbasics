a2b7b170a6364b33839ca8d4f01ecb0d9ca12166
JhosethE <160795626+JhosethE@users.noreply.github.com>
2024-02-25 13:38:04 -0500

    Update ejercicios19.java

--- ejercicio1/src/main/java/com/mycompany/ejercicio1/ejercicios19.java
+++ ejercicio1/src/main/java/com/mycompany/ejercicio1/ejercicios19.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.ejercicio1;
-
+import java.util.Scanner;
 /**
  *
  * @author L&J
  */
 public class ejercicios19 {
-    
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+    System.out.println("Ingrese la cantidad de respuestas correctas:");
+        int respuestasCorrectas = sc.nextInt();
+        
+        System.out.println("Ingrese la cantidad de respuestas incorrectas:");
+        int respuestasIncorrectas = sc.nextInt();
+        
+        System.out.println("Ingrese la cantidad de respuestas no contestadas:");
+        int respuestasNoContestadas = sc.nextInt();
+        
+        int notaFinal = respuestasCorrectas * 5 - respuestasIncorrectas;
+        
+        System.out.println("La nota final del estudiante es: " + notaFinal);
+    }
 }
